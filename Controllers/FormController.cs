bc65998c4f6ea8124383a88ae70cbb967a9c2a10
cristinambdev <cristinamb.dev@gmail.com>
2025-11-01 08:10:19 +0100

    form functionality added

--- Controllers/FormController.cs
+++ Controllers/FormController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Onatrix_Umbraco.Services;
 using Onatrix_Umbraco.ViewModels;
 using Umbraco.Cms.Core.Cache;
 using Umbraco.Cms.Core.Logging;
@@ -10,20 +11,24 @@ using Umbraco.Cms.Web.Website.Controllers;
 
 namespace Onatrix_Umbraco.Controllers
 {
-    public class FormController : SurfaceController
+    public class FormController(IUmbracoContextAccessor umbracoContextAccessor, IUmbracoDatabaseFactory databaseFactory, ServiceContext services, AppCaches appCaches, IProfilingLogger profilingLogger, IPublishedUrlProvider publishedUrlProvider, FormSubmissionsService formSubmissions) : SurfaceController(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
     {
-        public FormController(IUmbracoContextAccessor umbracoContextAccessor, IUmbracoDatabaseFactory databaseFactory, ServiceContext services, AppCaches appCaches, IProfilingLogger profilingLogger, IPublishedUrlProvider publishedUrlProvider) : base(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
-        {
-        }
-
+        private readonly FormSubmissionsService _formSubmissions = formSubmissions;
         public IActionResult HandleCallbackForm(CallbackFormViewModel model)
         {
             if (!ModelState.IsValid)
             {
                 return CurrentUmbracoPage();
             }
+            var result = _formSubmissions.SaveCallbackRequest(model);
+            if(!result)
+            {
+                TempData["FormError"] = "There was an error while submitting your request. Please try again later.";
+                //ModelState.AddModelError("", "There was an error saving your request. Please try again later.");
+                return RedirectToCurrentUmbracoPage();
+            }
+            TempData["FormSuccess"] = "Thank you! Your request has been received. We will get back to you soon";
 
-            // Work with form data here
 
             return RedirectToCurrentUmbracoPage();
         }
